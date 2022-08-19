4dffa874074c07990561ae3c1a1b6d857582535c
roxsampoerna <110806380+roxsampoerna@users.noreply.github.com>
2022-08-19 14:27:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
+ 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = 3000
 const fs = require('fs')
 const Connection = require('./modules/Connection')
 const ConnectionList = require('./modules/ConnectionList')
